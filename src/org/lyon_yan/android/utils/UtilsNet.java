ca3972f70e076cea7ad24cd870fd2e87f7a4cfab
Lyon Yan <765211630@qq.com>
2015-08-21 09:00:20 +0800

    22

--- src/org/lyon_yan/android/utils/UtilsNet.java
+++ src/org/lyon_yan/android/utils/UtilsNet.java
@@ -13,8 +13,9 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 
+@SuppressWarnings("deprecation")
 public class UtilsNet {
-	public static String getLocalIpAddress() {
+	public static String getLocalIpV6Address() {
 		try {
 			for (Enumeration<NetworkInterface> en = NetworkInterface
 					.getNetworkInterfaces(); en.hasMoreElements();) {
@@ -33,40 +34,65 @@ public class UtilsNet {
 		return null;
 	}
 
-	// 得到本机ip地址
-	public static String getLocalHostIp() {
-		String ipaddress = "";
+	public static String getLocalIpV4Address() {
+
 		try {
-			Enumeration<NetworkInterface> en = NetworkInterface
-					.getNetworkInterfaces();
-			// 遍历所用的网络接口
-			while (en.hasMoreElements()) {
-				NetworkInterface nif = en.nextElement();// 得到每一个网络接口绑定的所有ip
-				Enumeration<InetAddress> inet = nif.getInetAddresses();
-				// 遍历每一个接口绑定的所有ip
-				while (inet.hasMoreElements()) {
-					InetAddress ip = inet.nextElement();
-					if (!ip.isLoopbackAddress()
-							&& InetAddressUtils.isIPv4Address(ip
+			for (Enumeration<NetworkInterface> en = NetworkInterface
+					.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf
+						.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()
+							&& InetAddressUtils.isIPv4Address(inetAddress
 									.getHostAddress())) {
-						return ipaddress = "本机的ip是" + "：" + ip.getHostAddress();
+						return inetAddress.getHostAddress().toString();
 					}
 				}
-
 			}
 		} catch (SocketException e) {
-			Log.e("feige", "获取本地ip地址失败");
-			e.printStackTrace();
+			// TODO: handle exception
+			Log.e("lyon_1", e.toString());
 		}
-		return ipaddress;
 
+		return null;
 	}
+
+	// // 得到本机ip地址
+	// public static String getLocalHostIp() {
+	// String ipaddress = "";
+	// try {
+	// Enumeration<NetworkInterface> en = NetworkInterface
+	// .getNetworkInterfaces();
+	// // 遍历所用的网络接口
+	// while (en.hasMoreElements()) {
+	// NetworkInterface nif = en.nextElement();// 得到每一个网络接口绑定的所有ip
+	// Enumeration<InetAddress> inet = nif.getInetAddresses();
+	// // 遍历每一个接口绑定的所有ip
+	// while (inet.hasMoreElements()) {
+	// InetAddress ip = inet.nextElement();
+	// if (!ip.isLoopbackAddress()
+	// && InetAddressUtils.isIPv4Address(ip
+	// .getHostAddress())) {
+	// return ipaddress = "本机的ip是" + "：" + ip.getHostAddress();
+	// }
+	// }
+	//
+	// }
+	// } catch (SocketException e) {
+	// Log.e("feige", "获取本地ip地址失败");
+	// e.printStackTrace();
+	// }
+	// return ipaddress;
+	//
+	// }
 
 	// 得到本机Mac地址
 	public static String getLocalMac(Activity activity) {
 		String mac = "";
 		// 获取wifi管理器
-		WifiManager wifiMng = (WifiManager) activity.getSystemService(Context.WIFI_SERVICE);
+		WifiManager wifiMng = (WifiManager) activity
+				.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfor = wifiMng.getConnectionInfo();
 		mac = "本机的mac地址是：" + wifiInfor.getMacAddress();
 		return mac;
