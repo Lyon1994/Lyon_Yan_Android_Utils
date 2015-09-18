d94662d245e3e3a966369b5fa95f15f5a15ea8b8
Lyon_Yan <765211630@qq.com>
2015-09-18 17:49:04 +0800

    121

--- src/org/lyon_yan/android/utils/volley/Config.java
+++
@@ -1,9 +0,0 @@
-package org.lyon_yan.android.utils.volley;
-
-import com.android.volley.DefaultRetryPolicy;
-
-public class Config {
-
-	public static DefaultRetryPolicy DefaultRetryPolicy = new DefaultRetryPolicy(
-			2 * 1000, 1, 1.0f);
-}
