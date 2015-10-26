654ce963d71f992a1c76025159312aff1c97f712
Lyon_Yan <765211630@qq.com>
2015-10-26 17:38:32 +0800

    快速复制文件夹的所有内容至指定目录

--- src/org/lyon_yan/android/utils/file/FileUtill.java
+++ src/org/lyon_yan/android/utils/file/FileUtill.java
@@ -1,9 +1,11 @@
 package org.lyon_yan.android.utils.file;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,5 +66,75 @@ public class FileUtill {
 			}
 		}
 		return filess;
+	}
+
+	/**
+	 * 快速复制文件夹的所有内容至指定目录
+	 * 
+	 * @author Lyon_Yan <br/>
+	 *         <b>time</b>: 2015年10月26日 下午5:37:17
+	 * @param from_path
+	 * @param to_path
+	 */
+	public static void copyFiles(String from_path, String to_path) {
+		File dir = new File(from_path);
+		File to_dir = new File(to_path);
+		if (to_dir.exists()) {
+			to_dir.delete();
+		}
+		to_dir.mkdirs();
+		if (dir.isDirectory()) {
+			File[] files = dir.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					// 这里面用了递归的算法
+					String temp = File.pathSeparator + file.getName();
+					copyFiles(from_path + temp, to_path + temp);
+				} else {
+					fileChannelCopy(dir, to_dir);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * 使用文件通道的方式复制文件
+	 * 
+	 * 
+	 * 
+	 * @param s
+	 * 
+	 *            源文件
+	 * 
+	 * @param t
+	 * 
+	 *            复制到的新文件
+	 */
+
+	public static void fileChannelCopy(File s, File t) {
+		FileInputStream fi = null;
+		FileOutputStream fo = null;
+		FileChannel in = null;
+		FileChannel out = null;
+		try {
+			fi = new FileInputStream(s);
+			fo = new FileOutputStream(t);
+			in = fi.getChannel();// 得到对应的文件通道
+			out = fo.getChannel();// 得到对应的文件通道
+			in.transferTo(0, in.size(), out);// 连接两个通道，并且从in通道读取，然后写入out通道
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fi.close();
+				in.close();
+				fo.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
